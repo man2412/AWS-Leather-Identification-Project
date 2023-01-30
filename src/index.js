7ec4a5f4b245030adac4f51a427e96478f164ab0
Manav Tanna <f20201689@goa.bits-pilani.ac.in>
2023-01-30 18:28:51 +0530

    working with new ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+
 import  {Amplify}  from 'aws-amplify'
 import config from './aws-exports'
+import App from './App';
 Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

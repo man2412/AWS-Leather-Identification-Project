eeccbc1d1f1f772d6c936a8da4a0d566fbd9805c
Manav Tanna <f20201689@goa.bits-pilani.ac.in>
2023-01-30 16:46:42 +0530

    test remove auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,10 @@ export type AmplifyDependentResourcesAttributes = {
     "amplifyreactauth29422f09": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
